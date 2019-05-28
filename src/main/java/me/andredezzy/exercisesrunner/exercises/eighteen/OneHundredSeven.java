b747ddfef2d642ed33e39291a021f86fc4889152
André <andredezzy@outlook.com>
2019-05-28 17:13:24 -0300

    :tada: start exercises eighteen

---
+++ src/main/java/me/andredezzy/exercisesrunner/exercises/eighteen/OneHundredSeven.java
@@ -0,0 +1,23 @@
+package me.andredezzy.exercisesrunner.exercises.eighteen;
+
+import javax.swing.JOptionPane;
+import me.andredezzy.exercisesrunner.exercises.Exercise;
+
+public class OneHundredSeven implements Exercise {
+
+    public void run(String[] args) {
+        int lastNumber = 0;
+        int highestNumber = 0;
+
+        while (lastNumber != 9999) {
+            String numberString = JOptionPane.showInputDialog("Informe um número (9999 para parar): ");
+            lastNumber = Integer.valueOf(numberString);
+
+            if (lastNumber != 9999 && lastNumber > highestNumber) {
+                highestNumber = lastNumber;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "O maior número dentre os que você digitou, é: " + highestNumber);
+    }
+}
