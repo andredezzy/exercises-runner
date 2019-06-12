6dd480ff12ea7f29e7a458c661589d7ec2d197ab
André <andredezzy@outlook.com>
2019-06-12 14:07:03 -0300

    :tada: start exercises nineteen

---
+++ src/main/java/me/andredezzy/exercisesrunner/exercises/nineteen/One.java
@@ -0,0 +1,9 @@
+package me.andredezzy.exercisesrunner.exercises.nineteen;
+
+import me.andredezzy.exercisesrunner.exercises.Exercise;
+
+public class One implements Exercise {
+
+    public void run(String[] args) {
+    }
+}
