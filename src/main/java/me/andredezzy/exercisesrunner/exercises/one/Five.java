ae1d3c3002706c83346be2d431f5978a5be83e4f
André 'Dezzy <andrevcv1@gmail.com>
2020-08-28 20:36:59 -0300

    chore: Add exercise 'Five'

---
+++ src/main/java/me/andredezzy/exercisesrunner/exercises/one/Five.java
@@ -0,0 +1,18 @@
+package me.andredezzy.exercisesrunner.exercises.one;
+
+import me.andredezzy.exercisesrunner.Application;
+import me.andredezzy.exercisesrunner.exercises.Exercise;
+
+public class Five implements Exercise {
+    public void run(String[] args) {
+        System.out.print("Informe um valor: ");
+        double value = Application.ASKER.askForDouble();
+
+        System.out.print("Informe uma porcentagem de juros à ser acrescentada: ");
+        double percentage = Application.ASKER.askForPercentage();
+
+        double finalValue = value * percentage;
+
+        System.out.printf("%nValor final: R$ %s.%n", finalValue);
+    }
+}
